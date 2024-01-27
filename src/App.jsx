80754e7b2d7805516bcc3ae65229145158db2af5
Shahin M. Shahin <41402573+peregrineshahin@users.noreply.github.com>
2024-01-27 20:29:34 +0300

    Refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { MainContextProvider } from "./Components/Context/Context";
+import InputUCI from "./Components/InputUCI/InputUCI";
+
 import "./App.scss";
 import WebWorker from "./Components/WebWorker/WebWorker";
-import Textarea from "react-expanding-textarea";
+import MainContent from "./Components/MainContent/MainContent";
 
 function App() {
   const [userInput, setUserInput] = useState("");
@@ -11,32 +13,11 @@ function App() {
 
   const divRef = useRef();
 
-  const handleSubmit = (e) => {
-    if (e) e.preventDefault();
-    if (userInput) setUciInput(userInput);
-  };
-
-  const textareaRef = useRef(null);
-
-  const handleKeyPress = (e) => {
-    // Check if the pressed key is Enter (keyCode 13)
-    if (e.key === "Enter") {
-      // Prevent the default behavior of the Enter key (e.g., adding a newline)
-      e.preventDefault();
-      // Perform the form submission logic here
-      handleSubmit();
-    }
-  };
-
   useEffect(() => {
     // Scroll down the div whenever the text content changes
     if (divRef && divRef.current && result)
       divRef.current.scrollTop = divRef.current.scrollHeight;
   }, [result]);
-
-  const handleChange = useCallback((e) => {
-    setUserInput(e.target.value);
-  }, []);
 
   return [
     <MainContextProvider
@@ -55,45 +36,9 @@ function App() {
           <div className="main-container">
             <div className="container" ref={divRef}>
               <div className="title">Stockfish Worker React.js Example</div>
+              <MainContent />
               <WebWorker workerLink="/stockfish/src/stockfish-nnue-16.js" />
-              <div className="form-wrapper">
-                <form onSubmit={(e) => handleSubmit(e)}>
-                  <div className="uciTextArea">
-                    <Textarea
-                      className="textarea"
-                      type="submit"
-                      id="uci_textarea"
-                      maxLength="3000"
-                      style={{ minHeight: "52px" }}
-                      name="uci_textarea"
-                      onChange={handleChange}
-                      onKeyDown={(e) => handleKeyPress(e)}
-                      placeholder="Enter UCI input"
-                      ref={textareaRef}
-                      value={userInput}
-                    />
-
-                    <button type="submit" disabled="" data-testid="send-button">
-                      <span>
-                        <svg
-                          width="24"
-                          height="24"
-                          viewBox="0 0 24 24"
-                          fill="none"
-                        >
-                          <path
-                            d="M7 11L12 6L17 11M12 18V7"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                          ></path>
-                        </svg>
-                      </span>
-                    </button>
-                  </div>
-                </form>
-              </div>
+              <InputUCI />
             </div>
           </div>
         </main>
---
+++ src/Components/InputUCI/InputUCI.jsx
@@ -0,0 +1,67 @@
+import React, { useCallback, useContext, useRef } from "react";
+import "./InputUCI.scss";
+import MainContext from "../Context/Context";
+import Textarea from "react-expanding-textarea";
+
+function InputUCI(props) {
+  const context = useContext(MainContext);
+
+  const handleSubmit = (e) => {
+    if (e) e.preventDefault();
+    if (context.userInput) context.setUciInput(context.userInput);
+  };
+
+  const textareaRef = useRef(null);
+
+  const handleKeyPress = (e) => {
+    // Check if the pressed key is Enter (keyCode 13)
+    if (e.key === "Enter") {
+      // Prevent the default behavior of the Enter key (e.g., adding a newline)
+      e.preventDefault();
+      // Perform the form submission logic here
+      handleSubmit();
+    }
+  };
+
+  const handleChange = useCallback((e) => {
+    context.setUserInput(e.target.value);
+  }, []);
+
+  return (
+    <div className="form-wrapper">
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <div className="uciTextArea">
+          <Textarea
+            className="textarea"
+            type="submit"
+            id="uci_textarea"
+            maxLength="3000"
+            style={{ minHeight: "52px" }}
+            name="uci_textarea"
+            onChange={handleChange}
+            onKeyDown={(e) => handleKeyPress(e)}
+            placeholder="Enter UCI input"
+            ref={textareaRef}
+            value={context.userInput}
+          />
+
+          <button type="submit" disabled="" data-testid="send-button">
+            <span>
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+                <path
+                  d="M7 11L12 6L17 11M12 18V7"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></path>
+              </svg>
+            </span>
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default InputUCI;
